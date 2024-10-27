7a2af5c3dfb1f6659e3158ccfb9b106729ea22b1
jamos <amosjoel91@gmail.com>
2024-10-27 17:35:48 +0300

    feat: configure root route and html paths

--- api/routes.js
+++ api/routes.js
@@ -20,6 +20,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 const htmlFiles = ['contact', 'certifications', 'socials', 'blogs'];
 
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route to render HTML files
+htmlFiles.forEach(file => {
+    app.get(`/${file}`, (req, res) => {
+        const filePath = path.join(__dirname, 'public', `${file}.html`);
+        res.sendFile(filePath, err => {
+            if (err) {
+                console.error(`Error serving ${file}.html:`, err);
+                res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+            }
+        });
+    });
+});
 
 // server setup
 const PORT = process.env.PORT || 3000;
