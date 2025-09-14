f2b87c994b35966e1c33205c29e0545a88ca7352
Murzuqisah <amosjoel229@gmail.com>
2025-09-14 23:58:46 +0300

    feat(security): allow font awesome css in csp

--- middleware/security.js
+++ middleware/security.js
@@ -4,7 +4,7 @@ export const helmetConfig = helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdn.jsdelivr.net"],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdn.jsdelivr.net", "https://ka-f.fontawesome.com"],
             scriptSrc: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com", "https://ajax.googleapis.com", "https://cdnjs.cloudflare.com"],
             imgSrc: ["'self'", "data:", "https:"],
             connectSrc: ["'self'"],
